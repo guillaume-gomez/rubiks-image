0b3feb1da4d34e74122b6fb1a39b4336879979a8
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-11-13 10:17:03 +0100

    change color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,27 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2c5da6",
+          "secondary": "#bd2827",
+          "accent": "#7cb257",
+          "neutral": "#eecf4e",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+
+          "--rounded-box": "0rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0em", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "0rem", // border radius rounded-badge utility class, used in badges and similar
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
 
