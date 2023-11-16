866e43ef60b3914d5a2f204a119daa074de93bd8
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-11-16 09:16:52 +0100

    update the header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,9 @@
+import logo from '/logo.svg'
+
 function Header() {
   return (
     <div className="navbar bg-primary text-primary-content">
+      <img src={logo} alt="logo of Rubick image" width="64px"/>
       <a className="btn btn-ghost normal-case text-xl">Rubick's Image</a>
     </div>
   );
