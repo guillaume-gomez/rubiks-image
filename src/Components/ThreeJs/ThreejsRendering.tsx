0e96f3a86db8ee4a93f100313c3d3e64d63992bd
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-12-20 06:36:03 +0100

    update the scene

--- src/Components/ThreeJs/ThreejsRendering.tsx
+++ src/Components/ThreeJs/ThreejsRendering.tsx
@@ -1,9 +1,10 @@
 import { useRef , useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { CameraControls, Stats, GizmoHelper, GizmoViewport } from '@react-three/drei';
+import { CameraControls, Stats, GizmoHelper, GizmoViewport, Backdrop, Plane, SpotLight } from '@react-three/drei';
 import { RubickFace } from "../../types";
 import Toggle from "../Toggle";
-import CubesInstanceMesh from "./CubesInstanceMesh";
+import RubickCubesInstanceMesh from "./RubickCubesInstanceMesh";
+import CubesSingleLayerInstanceMesh from "./CubesSingleLayerInstanceMesh";
 
 interface ThreejsRenderingProps {
   width: number;
@@ -18,6 +19,13 @@ function ThreejsRendering({ width, height, tileSize, rubickFaces, hasBorder, tog
   const cameraControlRef = useRef<CameraControls|null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [hideOtherFaces, setHideOtherFaces] = useState<boolean>(false);
+
+
+  useEffect(() => {
+    if(cameraControlRef && cameraControlRef.current) {
+      cameraControlRef.current.dollyTo(height/tileSize * 2 , true);
+    }
+  }, [])
 
   useEffect(() => {
     if(cameraControlRef && cameraControlRef.current) {
@@ -41,7 +49,7 @@ function ThreejsRendering({ width, height, tileSize, rubickFaces, hasBorder, tog
   return (
     <div>
       <Toggle
-        label="Hide other faces (improve performences)"
+        label="Hide other faces (improve performances)"
         value={hideOtherFaces}
         toggle={() => setHideOtherFaces(!hideOtherFaces)}
       />
@@ -53,15 +61,38 @@ function ThreejsRendering({ width, height, tileSize, rubickFaces, hasBorder, tog
           ref={canvasRef}
           style={{width: "75%", height: "90%"}}
         >
-          <color attach="background" args={['skyblue']} />
+          <color attach="background" args={['#0f1216']} />
           <Stats/>
           <ambientLight args={[0xffffff]} intensity={0.5} position={[0, 0.5, 0.5]} />
-          <directionalLight position={[0, 0, 5]} intensity={0.5} />
+          <directionalLight position={[-2,0, 0]} intensity={0.50} />
+          <directionalLight position={[ 2,0, 0]} intensity={0.50} />
+          <directionalLight color={0xffffff}  position={[ 0,0, 5]} intensity={0.5} />
+          {/*<SpotLight
+            distance={100}
+            angle={1}
+            attenuation={5}
+            anglePower={Math.PI/2}
+            position={[0,0,15]}
+         />*/}
+          <Backdrop
+            scale={[100,100,25]}
+            position={[0, -(height/2/tileSize) -5, -10]}
+            floor={3} // Stretches the floor segment, 0.25 by default
+            segments={20} // Mesh-resolution, 20 by default
+          >
+            <meshStandardMaterial color="#FFFFFF" metalness={1.0} emissive={"#45A5FF"} flatShading={true} />
+          </Backdrop>
+          {/*<Plane args={[200, 200]} rotation-x={-Math.PI/2} />*/}
+
             <group
               position={[
               -(width/2/tileSize), (height/2/tileSize), 0]}
             >
-              <CubesInstanceMesh tileSize={tileSize} rubickFaces={rubickFaces} />
+              {
+                hideOtherFaces ?
+                <CubesSingleLayerInstanceMesh tileSize={tileSize} rubickFaces={rubickFaces} />
+                : <RubickCubesInstanceMesh tileSize={tileSize} rubickFaces={rubickFaces} />
+              }
 
             </group>
               <GizmoHelper alignment="bottom-right" margin={[50, 50]}>
