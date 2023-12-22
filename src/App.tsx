fea912aca05f4302783723cb851cc352f994c284
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-12-22 16:38:36 +0100

    add info message about the number of cubes and the performances

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,7 @@ function App() {
                       disabled={bestProportion}
                       type="range"
                       min={3}
-                      max={24}
+                      max={9}
                       step={3}
                       value={ratio}
                       onChange={(e) => setRatio(parseInt(e.target.value))}
@@ -172,6 +172,14 @@ function App() {
                     <span>{ratio}</span>
                   </div>
                 </div>
+                {
+                  ((possibleWidth/tileSize) * (possibleHeight/tileSize)) > 5000 ?
+                    <div role="alert" className="alert alert-warning">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                      <span>Warning: The number of cubes can be too much in 3D</span>
+                    </div>
+                   : <></>
+                }
                 {renderPreview()}
               </div>
             </Card>
