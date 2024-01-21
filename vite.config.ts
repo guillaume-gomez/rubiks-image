fb2614065ccfa14898a8cd03ba0f349aa8221e63
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-01-21 20:55:21 +0100

    install bundler helper + depolyment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import checker from 'vite-plugin-checker'
+import { defineConfig, type PluginOption } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
+import {visualizer } from 'rollup-plugin-visualizer';
+import { splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rubicks-image",
-  plugins: [react(), checker({typescript: true}) ],
+  base: "/rubiks-image/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if(id.includes('three') || id.includes('@react-three')) {
+            return 'three';
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+      react(),
+      checker({typescript: true}),
+      splitVendorChunkPlugin(),
+      //visualizer({ filename: 'bundle-analysis.html', open: true }) as PluginOption,
+  ]
 })
