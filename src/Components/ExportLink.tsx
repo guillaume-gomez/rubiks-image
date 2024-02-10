f1bfc7e4e008b6285163118dd5956992f940ea03
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-02-10 18:47:13 +0100

    finish to create export button

--- src/Components/ExportLink.tsx
+++ src/Components/ExportLink.tsx
@@ -1,23 +1,41 @@
+import { useState, useEffect } from "react";
+
 interface ExportLinkProps {
   label: string;
   params: string;
 }
 
 function ExportLink({ label, params } : ExportLinkProps) {
-  
+  const [displayIndicator, setDisplayIndicator] = useState<boolean>(false);
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  useEffect(() => {
+    if(clicked) {
+      setDisplayIndicator(true);
+      setTimeout(() => {
+        setDisplayIndicator(false);
+        setClicked(false);
+      }, 5000);
+    }
+  }, [clicked]);
+
   function exportLink() {
     const url = `${window.location.origin}?${params}`;
     navigator.clipboard.writeText(url);
+
+    setClicked(true);
   }
 
-
   return (
-    <button
-      className="btn btn-primary"
-      onClick={exportLink}
-    >
-      {label}
+    <div className="indicator">
+      {displayIndicator && <span className="indicator-item badge badge-primary">Copied !</span>}
+      <button
+        className="btn btn-primary"
+        onClick={exportLink}
+      >
+        {label}
     </button>
+    </div>
   );
 }
 
